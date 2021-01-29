8329d2723386108bf5bb58a2dabfb9a687e1e933
mixueshi <631161722@qq.com>
2021-01-29 17:25:45 +0800

    mxs完成了个人记录修改模块

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net';
 // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
     // 在发送请求之前做些什么
-    console.log(config);
+
     const token = localStorage.getItem('token')
     if (config.url.startsWith('/my')) {
         config.headers.Authorization = token
